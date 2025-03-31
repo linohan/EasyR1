0c19589ba45f466c4ad6bd07889fb7725d0645eb
jiaming.lin <jiaming.lin@ly.com>
2025-03-31 14:23:41 +0800

    修改模型好和数据存储

--- examples/qwen2_5_7b_math_multi_node.sh
+++ examples/qwen2_5_7b_math_multi_node.sh
@@ -3,13 +3,13 @@ set -x
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export VLLM_USE_V1=0
 
-MODEL_PATH=/data_nvme1n1/work/ljm/model/Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
+MODEL_PATH=../../model/Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
 
 SYSTEM_PROMPT="""You FIRST think about the reasoning process as an internal monologue and then provide the final answer.
  The reasoning process MUST BE enclosed within <think> </think> tags. The final answer MUST BE put in \boxed{}."""
 
 
-ray job submit --address="http://127.0.0.1:8265" \
+ray job submit --address="http://10.242.64.12:8265" \
     --runtime-env=examples/runtime_env.yaml \
     --no-wait \
     -- \
