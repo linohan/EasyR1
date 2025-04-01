4d06078f03a662b6f4c849dfe9a1bfc8c72dd62a
jiaming.lin <jiaming.lin@ly.com>
2025-04-01 15:30:28 +0800

    更新配置

--- examples/qwen2_5_7b_math.sh
+++ examples/qwen2_5_7b_math.sh
@@ -3,7 +3,7 @@ set -x
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export VLLM_USE_V1=0
 
-MODEL_PATH=/data_nvme1n1/work/ljm/model/Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
+MODEL_PATH=/data_nvme3n1/work/ljm/model/Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
 
 SYSTEM_PROMPT="""You FIRST think about the reasoning process as an internal monologue and then provide the final answer.
  The reasoning process MUST BE enclosed within <think> </think> tags. The final answer MUST BE put in \boxed{}."""
