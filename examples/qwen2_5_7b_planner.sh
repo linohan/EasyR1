0d2f93fd5d2d96b1dcdae698fd6158e125ad0e75
jiaming.lin <jiaming.lin@ly.com>
2025-05-07 15:38:59 +0800

    修改ray启动

--- examples/qwen2_5_7b_planner.sh
+++ examples/qwen2_5_7b_planner.sh
@@ -7,7 +7,6 @@ export PYTHONUNBUFFERED=1
 MODEL_PATH=/data_nvme3n1/model/Qwen/Qwen2.5-7B-Instruct  # replace it with your local file path
 
 ray job submit --address="http://10.242.64.07:8265" \
-    --runtime-env=examples/runtime_env.yaml \
     --no-wait \
     -- \
     python3 -m verl.trainer.main \
