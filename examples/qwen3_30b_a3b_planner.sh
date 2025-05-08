49b313ec239f721e3dd694ae22003f0b23f7d5ab
jiaming.lin <jiaming.lin@ly.com>
2025-05-08 13:50:11 +0800

    增加Qwen3-30B-A3B配置

--- examples/qwen3_30b_a3b_planner.sh
+++ examples/qwen3_30b_a3b_planner.sh
@@ -10,5 +10,15 @@ ray job submit --address="http://10.242.64.07:8265" \
     --no-wait \
     -- \
     python3 -m verl.trainer.main \
-        config=examples/qwen3_30b_a3b_planner_config.yaml \
-        worker.actor.model.model_path=${MODEL_PATH}
+        config=examples/qwen2_5_7b_planner_config.yaml \
+        data.train_files=/data_nvme3n1/dataset/manual_corrected-20250311@train \
+        data.val_files=/data_nvme3n1/dataset/manual_corrected-20250311@test \
+        worker.actor.model.model_path=${MODEL_PATH} \
+        worker.actor.micro_batch_size_per_device_for_update=1 \
+        worker.actor.micro_batch_size_per_device_for_experience=8 \
+        worker.actor.fsdp.torch_dtype=bf16 \
+        worker.actor.optim.strategy=adamw_bf16 \
+        worker.rollout.tensor_parallel_size=8 \
+        trainer.experiment_name=qwen3_30b_a3b_planner \
+        trainer.n_gpus_per_node=8
+
