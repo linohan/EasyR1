2922d53a189bddd8560758dc3a4010b062caa249
jiaming.lin <jiaming.lin@ly.com>
2025-05-09 19:36:40 +0800

    改为固定ip

--- examples/qwen3_8b_planner_one_json.sh
+++ examples/qwen3_8b_planner_one_json.sh
@@ -15,7 +15,7 @@ get_ip() {
 
 MODEL_PATH=/data_nvme3n1/model/Qwen/Qwen3-8B  # replace it with your local file path
 
-ray job submit --address="http://$(get_ip):8265" \
+ray job submit --address="http://10.242.64.12:8265" \
     --no-wait \
     -- \
     python3 -m verl.trainer.main \
