a6d3b192fab7c09525c6bae537ee6723b1762ae5
jiaming.lin <jiaming.lin@ly.com>
2025-05-09 17:06:48 +0800

    动态ip

--- examples/qwen3_8b_planner_one_json.sh
+++ examples/qwen3_8b_planner_one_json.sh
@@ -4,9 +4,18 @@ set -x
 
 export PYTHONUNBUFFERED=1
 
+# 获取活跃网卡的IP
+get_ip() {
+    # 获取默认路由对应的网卡
+    local interface=$(ip route | awk '/default/ {print $5}')
+    # 获取该网卡的IP
+    local ip=$(ip -4 addr show $interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    echo $ip
+}
+
 MODEL_PATH=/data_nvme3n1/model/Qwen/Qwen3-8B  # replace it with your local file path
 
-ray job submit --address="http://10.242.64.12:8265" \
+ray job submit --address="http://$(get_ip):8265" \
     --no-wait \
     -- \
     python3 -m verl.trainer.main \
